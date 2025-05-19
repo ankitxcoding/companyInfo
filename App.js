8ff3de1ea753b3ed15825bfc74972b65dc95dc8a
ankitxcoding <ankitxcoding@gmail.com>
2025-05-19 23:07:40 +0530

    company info app created

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import "react-native-get-random-values";
 import { v4 as uuidv4 } from "uuid";
